1d7d9ca4e2a22698a216c45726f79f40fa2d5155
pawelgrimm <txtpawl@gmail.com>
2020-06-13 16:15:02 -0500

    updated font-styles, added movie icon to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Video } from "react-feather";
 
 export const Header = () => (
   <header className="header">
     <div className="content-container">
       <div className="header__content">
-        <Link
-          className="header__title"
-          to="/dashboard"
-        >
+        <Link className="header__title" to="/dashboard">
           <h1>Movie Night</h1>
+        </Link>
+        <Link className="header__button" to="/">
+          <Video className="vertical-align-middle" size={32} />
         </Link>
       </div>
     </div>
