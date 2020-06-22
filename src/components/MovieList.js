654ea35c28af542d3084224389330c767f1f5e4f
pawelgrimm <txtpawl@gmail.com>
2020-06-21 21:32:11 -0500

    Added greeting/instructions message

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,17 +6,31 @@ const mapStateToProps = ({ movies }) => ({ movies });
 
 export const MovieList = ({ movies, toggleModal }) => {
   return (
-    movies.length > 0 && (
-      <div className="list-body">
-        {movies.map((movie) => (
+    <div className="list-body">
+      {movies.length > 0 ? (
+        movies.map((movie) => (
           <MovieListItem
             key={movie.id}
             movie={movie}
             toggleModal={toggleModal}
           />
-        ))}
-      </div>
-    )
+        ))
+      ) : (
+        <div>
+          <h3>Welcome to Movie Night!</h3>
+          <p>Use the search to add some movies to your list.</p>
+          <p>
+            Once you're done, send the list to your friends. They will be able
+            to vote on the movies you selected!
+          </p>
+          <p>
+            When all the votes are in, the most favorable movie will be selected
+            for your movie night!
+          </p>
+          <p>Enjoy!</p>
+        </div>
+      )}
+    </div>
   );
 };
 
