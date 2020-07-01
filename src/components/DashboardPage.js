6467aaada2cb8ecdfa1125637363dc140b9c5c1f
pawelgrimm <txtpawl@gmail.com>
2020-07-01 14:15:46 -0400

    removed legacy hook-based state

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,24 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import MovieSearch from "./MovieSearch";
 import MovieList from "./MovieList";
 import VideoModal from "./VideoModal";
 
 export const DashboardPage = ({ appElement }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const toggleModal = () => {
-    console.log("toggleModal called. modalIsOpen =", modalIsOpen);
-    setModalIsOpen(!modalIsOpen);
-  };
   return (
     <div className="content-container">
       <MovieSearch numResults={5} />
-      <MovieList toggleModal={toggleModal} />
-      <VideoModal
-        modalIsOpen={modalIsOpen}
-        toggleModal={toggleModal}
-        appElement={appElement}
-      />
+      <MovieList />
+      <VideoModal appElement={appElement} />
     </div>
   );
 };
