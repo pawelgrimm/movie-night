523834a00cace05f2657131911cd8d2a226c2c8b
pawelgrimm <txtpawl@gmail.com>
2020-07-14 16:18:27 -0400

    more work on BallotMovieList

--- src/components/BallotMovieList.js
+++ src/components/BallotMovieList.js
@@ -2,10 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 import MovieListItem from "./MovieListItem";
 import { ThumbsUp, ThumbsDown } from "react-feather";
+import { openModal } from "../actions/videoModal";
 
 const mapStateToProps = ({ movies }) => ({ movies });
 
-export const BallotMovieList = ({ movies }) => {
+const mapDispatchToProps = (dispatch) => ({
+  openModal: (videos) => dispatch(openModal(videos)),
+});
+
+export const BallotMovieList = ({ movies, openModal }) => {
   return (
     <div className="list-body">
       {movies.map((movie) => (
@@ -13,42 +18,49 @@ export const BallotMovieList = ({ movies }) => {
           key={movie.id}
           movie={movie}
           collapsedButtons={collapsedButtons(movie.id)}
-          expandedButtons={expandedButtons(movie.id)}
+          expandedButtons={expandedButtons(movie.id, movie.trailers)}
         />
       ))}
     </div>
   );
 };
 
-export default connect(mapStateToProps)(BallotMovieList);
-
 const collapsedButtons = (movieId) => makeButtons(movieId, true);
 
-const expandedButtons = (movieId) =>
-  [
-    videos && videos.length > 0 && (
-      <button
-        className="button"
-        onClick={() => {
-          openModal(videos);
-        }}
-      >
-        Watch Trailer Watch Trailer
-      </button>
-    ),
-  ].append(makeButtons(movieId, false));
+const expandedButtons = (movieId, videos) => [
+  videos && videos.length > 0 && (
+    <button
+      className="button"
+      onClick={(event) => {
+        event.stopPropagation();
+        openModal(videos);
+      }}
+    >
+      Watch Trailer
+    </button>
+  ),
+  ...makeButtons(movieId, false),
+];
 
 const makeButtons = (movieId, collapsed = true) => [
   <button
     className="button"
-    onClick={() => console.log("thumbs up clicked for", movieId)}
+    onClick={(event) => {
+      event.stopPropagation();
+      console.log("thumbs up clicked for", movieId);
+    }}
   >
     <ThumbsUp />
   </button>,
   <button
     className="button"
-    onClick={() => console.log("thumbs up clicked for", movieId)}
+    onClick={(event) => {
+      event.stopPropagation();
+      console.log("thumbs up clicked for", movieId);
+    }}
   >
     <ThumbsDown />
   </button>,
 ];
+
+export default connect(mapStateToProps, mapDispatchToProps)(BallotMovieList);
