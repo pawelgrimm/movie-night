e8c53995dd4c25c51af80de3474bac35f9baa4a9
pawelgrimm <txtpawl@gmail.com>
2020-07-28 15:15:40 -0400

    finish VideoModal and update Modal styles

--- src/containers/Modal/components/VideoModal.js
+++ src/containers/Modal/components/VideoModal.js
@@ -1,28 +1,20 @@
-import React, { useCallback } from "react";
-import { useDispatch } from "react-redux";
-import ReactPlayer from "react-player";
-import { closeModal as closeModalAction } from "../service/actions";
+import React from "react";
+import VideoPlayer from "./VideoPlayer";
+import CloseModalButton from "./CloseModalButton";
+import ButtonGroup from "../../../components/ButtonGroup/ButtonGroup";
 
 export const VideoModal = ({ videos = [] }) => {
-  const dispatch = useDispatch();
-  const closeModal = useCallback(() => dispatch(closeModalAction()), []);
-
   const key = videos.length > 0 && videos[0].key;
   if (!key) {
     return;
   }
 
-  const videoUrl = `https://www.youtube.com/watch?v=${key}`;
   return (
     <>
-      <p>This is the new modal</p>
-      <ReactPlayer className="video-modal__video" url={videoUrl} />
-      <button
-        className="button button--secondary video-modal__button"
-        onClick={closeModal}
-      >
-        Close
-      </button>
+      <VideoPlayer videoKey={key} />
+      <ButtonGroup>
+        <CloseModalButton />
+      </ButtonGroup>
     </>
   );
 };
