5db0867e81752fa8c54cf9f4919515214ab3eebc
pawelgrimm <developer@pawelgrimm.com>
2020-08-10 17:04:08 -0400

    added some CSP and other useful headers

--- server/app.js
+++ server/app.js
@@ -1,24 +1,39 @@
 import path from "path";
 import express from "express";
+import helmet from "helmet";
+import expressStaticGzip from "express-static-gzip";
 import ballotRouter from "./routers/ballot";
 import voteRouter from "./routers/vote";
 import movieRouter from "./routers/movie";
 import appRoot from "app-root-path";
-import expressStaticGzip from "express-static-gzip";
 
 const app = express();
-const publicPath = path.join(appRoot.toString(), "public");
-console.log("publicPath:", publicPath);
-
 app.use(
-  "/",
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        baseUri: ["'self'"],
+        defaultSrc: ["'self'", "https://www.youtube.com"],
+        imgSrc: ["'self'", "https://image.tmdb.org"],
+        objectSrc: ["'none'"],
+        scriptSrc: ["'self'", "https://www.youtube.com", "https://s.ytimg.com"],
+        styleSrc: ["'self'", "https://fonts.googleapis.com"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
+        upgradeInsecureRequests: [],
+      },
+    },
+  })
+);
+app.disable("x-powered-by");
+
+const publicPath = path.join(appRoot.toString(), "public");
+app.use(
   expressStaticGzip(publicPath, {
     enableBrotli: true,
     orderPreference: ["br", "gz"],
   })
 );
 
-app.use(express.static(publicPath));
 app.use(express.json());
 app.use(ballotRouter);
 app.use(voteRouter);
