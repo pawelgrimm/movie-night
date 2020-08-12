f9b4aa10399808d821d4360d9987b92c45707c1b
pawelgrimm <developer@pawelgrimm.com>
2020-08-12 11:25:37 -0400

    fix layout (and add focus scrolling) to search

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,12 +1,12 @@
 import React from "react";
 import classNames from "classnames";
 
-const TextInput = ({ className, forwardRef, ...rest }) => {
+const TextInput = ({ className, containerRef, searchRef, ...rest }) => {
   return (
-    <div className="container--flex-col">
+    <div className="container--flex-col" ref={containerRef}>
       <input
         className={classNames("card text-input text-input--grow", className)}
-        ref={forwardRef}
+        ref={searchRef}
         {...rest}
       />
     </div>
