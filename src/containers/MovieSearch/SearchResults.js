431d574a8d7ffa41a866ed30fc899a3d7867d82f
pawelgrimm <developer@pawelgrimm.com>
2020-09-14 09:02:04 -0400

    refactored styles into modules, fixed overflow bug

--- src/containers/MovieSearch/SearchResults.js
+++ src/containers/MovieSearch/SearchResults.js
@@ -1,15 +1,18 @@
 import React from "react";
-import SearchResultItem from "./SearchResultItem";
+import SearchResultItem from "./SearchResultItem/SearchResultItem";
 import { useSearchContext } from "./search-context";
+import styles from "./movie-search.module.scss";
 
 export const SearchResults = () => {
   const { searchResults } = useSearchContext();
   return (
-    <div className="search-results">
-      <div className="dropdown">
-        {searchResults.map((movie) => (
-          <SearchResultItem key={movie.id} movie={movie} />
-        ))}
+    <div className="container--flex-col">
+      <div className="search-results">
+        <div className="dropdown">
+          {searchResults.map((movie) => (
+            <SearchResultItem key={movie.id} movie={movie} />
+          ))}
+        </div>
       </div>
     </div>
   );
